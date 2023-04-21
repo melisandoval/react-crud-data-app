3899fca54027cd6a8e2a1d8fdb5a78cd67d7d48f
melisand <melisandoval@live.com.ar>
2023-04-21 04:16:40 +0200

    update table component

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -7,25 +7,6 @@ import { DeleteSVG } from "../assets/svg/DeleteSVG";
 
 // eslint-disable-next-line react/prop-types
 export function DataTable({ data }) {
-  // eslint-disable-next-line react/prop-types
-  const tableData = data.map((element) => (
-    <tr key={element.id}>
-      <td>{element.first}</td>
-      <td>{element.last}</td>
-      <td>{element.email}</td>
-      <td>{element.company}</td>
-      <td>{element.country}</td>
-      <td className="d-flex">
-        <Button variant="secondary" className="m-1">
-          <EditSVG />
-        </Button>
-        <Button variant="secondary" className="m-1">
-          <DeleteSVG />
-        </Button>
-      </td>
-    </tr>
-  ));
-
   const tableHead = (
     <tr>
       <th>Nombre</th>
@@ -37,11 +18,30 @@ export function DataTable({ data }) {
     </tr>
   );
 
+  // eslint-disable-next-line react/prop-types
+  const tableBody = data.map((customer) => (
+    <tr key={customer.id}>
+      <td>{customer.first}</td>
+      <td>{customer.last}</td>
+      <td>{customer.email}</td>
+      <td>{customer.company}</td>
+      <td>{customer.country}</td>
+      <td className="d-flex">
+        <Button variant="secondary" className="m-1">
+          <EditSVG />
+        </Button>
+        <Button variant="secondary" className="m-1">
+          <DeleteSVG />
+        </Button>
+      </td>
+    </tr>
+  ));
+
   return (
     <section className="my-5 mx-2">
       <Table striped bordered hover>
         <thead>{tableHead}</thead>
-        <tbody>{tableData}</tbody>
+        <tbody>{tableBody}</tbody>
       </Table>
     </section>
   );
